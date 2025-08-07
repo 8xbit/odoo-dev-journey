38d7552fd96a44004cba11ae056e16aacdd628b8
omar <multitartanga@gmail.com>
2025-08-07 12:30:49 +0200

    [ADD] Create real estate basic Module stracture

---
+++ custom_addons/real_estate_tuto/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Property Management',#The only required field 
+    'version': '1.0',
+    'category': 'custom',
+    'summary': 'Manage properties',
+    'description': 'A simple module to manage properties.',
+    'author': 'omar',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
