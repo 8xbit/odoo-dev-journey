dd742a98d7892ca94f5fea29a3d989148186296f
omar <multitartanga@gmail.com>
2025-08-08 15:32:33 +0200

    [IMP] odoo fields and basic security basic menu basic view_

--- custom_addons/real_estate_tuto/__manifest__.py
+++ custom_addons/real_estate_tuto/__manifest__.py
@@ -1,12 +1,16 @@
 {
-    'name': 'Property Management',#The only required field 
+    'name': 'Property Management 2',#The only required field 
     'version': '1.0',
     'category': 'custom',
     'summary': 'Manage properties',
     'description': 'A simple module to manage properties.',
     'author': 'omar',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/estate_property_views.xml'
+        ],
     'installable': True,
     'application': True,
 }
--- custom_addons/real_estate_tuto/models/estate_property.py
+++ custom_addons/real_estate_tuto/models/estate_property.py
@@ -1,5 +1,25 @@
-from odoo import models
+from odoo import models,fields
 
 class EstateProperty(models.Model):
        _name = "estate.property"
        _description = "Real Estate Property"
+    
+       #Fields
+       name = fields.Char(required=True, string="Property Name")
+       description = fields.Text(string="Description")
+       postcode = fields.Char(string="Postcode")
+       date_availability = fields.Date(string="Availability Date")
+       expected_price = fields.Float(required=True, string="Expected Price")
+       bedrooms = fields.Integer(string="Bedrooms")
+       living_area = fields.Integer(string="Living Area (m2)")
+       garage = fields.Boolean(string="Has Garage")
+       garden = fields.Boolean(string="Has Garden")
+       state = fields.Selection(
+              selection=[
+              ('new', 'New'),
+              ('sold', 'Sold'),
+              ('canceled', 'Canceled'),
+              ],
+              string="Status",
+              default='new'
+       )
